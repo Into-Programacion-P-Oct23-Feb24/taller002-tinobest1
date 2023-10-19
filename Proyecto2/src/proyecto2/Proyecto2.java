e89d53ca0a111c6b633cc2af0602a9dcd6eb2e65
Tino Best <sayayinlenin@hotmail.com>
2023-10-19 16:48:34 -0500

    tecer commit, trabajando con git y GitHubClassroom

---
+++ Proyecto2/src/proyecto2/Proyecto2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto2;
+
+/**
+ *
+ * @author LAB.ELECT
+ */
+public class Proyecto2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("Ejemplo dos básico de Java\n" +
+"Versionado a través de Git\n" +
+"Manejado desde GitHub\n" +
+"Además se usar GitHubClassroom");
+    }
+    
+}
