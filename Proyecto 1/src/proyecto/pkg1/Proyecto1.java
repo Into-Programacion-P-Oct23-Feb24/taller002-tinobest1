f6648acd4ccb80f0e4809be2cd83b38caeea99c4
Tino Best <sayayinlenin@hotmail.com>
2023-10-19 16:31:15 -0500

    proyecto 01

---
+++ Proyecto 1/src/proyecto/pkg1/Proyecto1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto.pkg1;
+
+/**
+ *
+ * @author LAB.ELECT
+ */
+public class Proyecto1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+        System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+        
+    }
+    
+}
